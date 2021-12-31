a3e5bd1d9b2ff0dff230614cb6abfeace8419e9c
adam <adam@adamnvrtil.fun>
2021-12-31 15:34:06 +0100

    😋 progress in audiolatency 😋

--- audiolatency/audiolatency-arch.sh
+++ audiolatency/audiolatency-arch.sh
@@ -4,6 +4,9 @@
 ############################
 # This script should be continued after osu-install.sh
 
+# If user executed this script as root, warn him.
+[ "$USER" = "root" ] && printf "You can't execute this script as root.\n" && exit 1
+
 ## Functions
 errorterminal() { # This one outputs error in terminal.
 clear
@@ -41,23 +44,41 @@ zenity \
 	--no-wrap
 }
 
-help() { # This one will show help message.
-printf "This part of script \`osu-install\` will set audio latency to the lowest possible.\n\nARGUMENTS:\n-r/--revert: Will revert all changes that were done.\n-h/--help: Will show this message.\n"
-}
-
-revert() { # This one will revert any changes this script has done.
-}
-
 getpass() { # Check if sudo needs password, if yes, then something there's something wrong with sudoers.
 ! /usr/bin/sudo -n true >/dev/null 2>&1 && \
 	printf "We need your password, so we can continue further.\n" \
 	&& /usr/bin/sudo true && printf "Thank you for your collaboration, mate! "
 }
 
+help() { # This one will show help message.
+printf "This part of script \`osu-install\` will set audio latency to the lowest possible.\n\nARGUMENTS:\n-r/--revert: Will revert all changes that were done.\n-h/--help: Will show this message.\n"
+exit 0
+}
+
+#revert() { # This one will revert any changes this script has done.
+#exit 1
+#}
+
 ## Command arguments
 
 # This one checks command arguments.
-case "$2" in
+case "$@" in
 	--revert|-r) revert ;;
-	*) help ;;
+	--help|-h) help ;;
 esac
+
+# Make user use sudo, so the password won't be required again for this script.
+getpass
+
+# Wait for any user input.
+read -rsn1 -p"If you are ready, press any key."; clear
+
+# Remove `pulseaudio`, if pipewire is not installed, then install pipewire.
+! command -v "pipewire" && sudo pacman --noconfirm -Rdd pulseaudio >/dev/null 2>&1 && \
+	sudo pacman --noconfirm --needed -Sy pipewire pipewire-pulse pipewire-jack pipewire-alsa pipewire-media-session >/dev/null 2>&1
+
+# Check if `pipewire` has been installed.
+command -v "pipewire" || botherrors "PipeWire wasn't installed."
+
+# Enable `pipewire`
+systemctl enable --user pipewire >/dev/null 2>&1 || botherrors "PipeWire couldn't be enabled."
